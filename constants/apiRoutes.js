c1854730b7ff65244d20dd269e63bd3b263cfe1d
CosLynx AI <drixisgod@gmail.com>
2025-01-03 08:39:08 +0530

    generated file: `constants/apiRoutes.js` [api-calls, data-types]

---
+++ constants/apiRoutes.js
@@ -0,0 +1,9 @@
+// This file defines API endpoint routes
+const apiRoutes = {
+  login: '/auth/login',
+  register: '/auth/register',
+  goals: '/goals',
+  goalById: '/goals/:id',
+};
+
+export { apiRoutes };
